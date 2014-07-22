b09401ddfc9f3b87544c8f8364f81bfb62198978
Jonathan Maron <jonathan.a.maron@gmail.com>
2014-07-22 09:26:13 +0200

    Added Zend Framework 2 specific files

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tisd;
+
+class Module
+{
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+}
