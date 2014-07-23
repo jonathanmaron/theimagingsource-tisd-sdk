09fb69aa8915609cba70c64a86e1c042f09cc44b
Jonathan Maron <jonathan.a.maron@gmail.com>
2014-07-23 07:12:11 +0200

    Slight formatting changes

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,13 +1,10 @@
 <?php
 
 return array(
-
-    'Tisd\Sdk'                                  => __DIR__ . '/src/Tisd/Sdk.php',
-    'Tisd\Sdk\Cache'                            => __DIR__ . '/src/Tisd/Sdk/Cache.php',
-    'Tisd\Sdk\Helper'                           => __DIR__ . '/src/Tisd/Sdk/Helper.php',
-
+    'Tisd\Sdk'                                      => __DIR__ . '/src/Tisd/Sdk.php',
+    'Tisd\Sdk\Cache'                                => __DIR__ . '/src/Tisd/Sdk/Cache.php',
+    'Tisd\Sdk\Helper'                               => __DIR__ . '/src/Tisd/Sdk/Helper.php',
     'Tisd\Sdk\Exception\ExceptionInterface'         => __DIR__ . '/src/Tisd/Sdk/Exception/ExceptionInterface.php',
     'Tisd\Sdk\Exception\InvalidArgumentException'   => __DIR__ . '/src/Tisd/Sdk/Exception/InvalidArgumentException.php',
     'Tisd\Sdk\Exception\RuntimeException'           => __DIR__ . '/src/Tisd/Sdk/Exception/RuntimeException.php',
-
 );
