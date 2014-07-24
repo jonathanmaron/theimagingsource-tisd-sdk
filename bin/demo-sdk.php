02a88789207a8eb5577c0479fad95575592cf9bf
Jonathan Maron <jonathan.a.maron@gmail.com>
2014-07-24 08:51:30 +0200

    Samples now use autoload_legacy.php

--- bin/demo-sdk.php
+++ bin/demo-sdk.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/init_autoloader.php';
+include_once __DIR__ . '/../autoload_legacy.php';
 
 use Tisd\Sdk as Sdk;
 
--- bin/init_autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$map = include __DIR__ . '/../autoload_classmap.php';
-
-foreach ($map as $filename) {
-    include_once $filename;
-}
