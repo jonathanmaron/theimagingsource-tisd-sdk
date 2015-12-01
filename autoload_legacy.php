7599d09a9f01dc7cd7aa13b9cfcd9d6379dfec3e
Jonathan Maron <jonathan.a.maron@gmail.com>
2015-12-01 05:58:22 +0100

    Updating project structure

--- autoload_legacy.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$classmap = include_once __DIR__ . '/autoload_classmap.php';
-
-foreach ($classmap as $filename) {
-    include_once $filename;
-}
---
+++ src/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tisd;
+
+class Module
+{
+
+//    public function getAutoloaderConfig()
+//    {
+//        return array(
+//            'Zend\Loader\ClassMapAutoloader' => array(
+//                __DIR__ . '/autoload_classmap.php',
+//            ),
+//        );
+//    }
+
+    public function getConfig()
+    {
+        return array();
+    }
+
+}
