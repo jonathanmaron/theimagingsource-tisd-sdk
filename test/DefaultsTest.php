f22881e405b1e91884db716fb1aa8cb3c144d538
Jonathan Maron <jonathan.a.maron@gmail.com>
2015-12-28 13:49:37 +0100

    Updated tests

--- test/DefaultsTest.php
+++ test/DefaultsTest.php
@@ -3,22 +3,70 @@
 namespace TisdTest\Sdk;
 
 use PHPUnit_Framework_TestCase;
-
-use Tisd\Sdk\Defaults as TisdSdkDefaults;
+use Tisd\Defaults;
 
 class DefaultsTest extends PHPUnit_Framework_TestCase
 {
 
-    protected function setUp()
+    public function testGetContext()
     {
+        $this->assertNull(Defaults::getContext());
     }
 
-    protected function tearDown()
+    public function testGetHostname()
     {
+        $this->assertEquals(Defaults::HOSTNAME_PRODUCTION, Defaults::getHostname());
     }
 
-    public function testComingSoon()
+    public function testGetLocale()
     {
-        $this->markTestSkipped();
+        $this->assertEquals(Defaults::LOCALE, Defaults::getLocale());
     }
+
+    public function testGetTimeout()
+    {
+        $this->assertEquals(Defaults::TIMEOUT, Defaults::getTimeout());
+    }
+
+    public function testGetVersion()
+    {
+        $this->assertEquals(Defaults::VERSION, Defaults::getVersion());
+    }
+
+    public function testSetAndGetContext()
+    {
+        Defaults::setContext(Defaults::CONTEXT_MACHINE_VISION);
+
+        $this->assertEquals(Defaults::CONTEXT_MACHINE_VISION, Defaults::getContext());
+    }
+
+    public function testSetAndGetHostname()
+    {
+        Defaults::setHostname(Defaults::HOSTNAME_PRODUCTION);
+
+        $this->assertEquals(Defaults::HOSTNAME_PRODUCTION, Defaults::getHostname());
+    }
+
+    public function testSetAndGetLocale()
+    {
+        Defaults::setLocale(Defaults::LOCALE);
+
+        $this->assertEquals(Defaults::LOCALE, Defaults::getLocale());
+    }
+
+    public function testSetAndGetTimeout()
+    {
+        Defaults::setTimeout(Defaults::TIMEOUT);
+
+        $this->assertEquals(Defaults::TIMEOUT, Defaults::getTimeout());
+    }
+
+    public function testSetAndGetVersion()
+    {
+        Defaults::setVersion(Defaults::VERSION);
+
+        $this->assertEquals(Defaults::VERSION, Defaults::getVersion());
+    }
+
+
 }
