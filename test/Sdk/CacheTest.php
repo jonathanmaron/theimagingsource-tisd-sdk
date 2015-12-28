1e6cee674103373e66120f3b6e1f1db5a953e07a
Jonathan Maron <jonathan.a.maron@gmail.com>
2015-12-28 10:31:41 +0100

    Adding tests for Cache

--- test/Sdk/CacheTest.php
+++ test/Sdk/CacheTest.php
@@ -3,22 +3,72 @@
 namespace TisdTest\Sdk;
 
 use PHPUnit_Framework_TestCase;
-
-use Tisd\Sdk\Cache as TisdSdkCache;
+use Tisd\Sdk\Cache;
 
 class CacheTest extends PHPUnit_Framework_TestCase
 {
+    protected $cache;
 
     protected function setUp()
     {
+        $this->cache = new Cache;
     }
 
     protected function tearDown()
     {
+        unset($this->cache);
     }
 
-    public function testComingSoon()
+    public function testGetFilename()
     {
         $this->markTestSkipped();
     }
+
+    public function testGetId()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testGetUser()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testPurge()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testRead()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testSetAndGetPath()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testSetAndGetTtl()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testWrite()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testWriteUnlinkFile()
+    {
+        $cacheId = 'cache-id';
+        $data    = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        $ret1 = $this->cache->write($cacheId, $data);
+        $ret2 = $this->cache->write($cacheId, $data);
+
+        $this->assertTrue(is_integer($ret1));
+        $this->assertTrue(is_integer($ret2));
+    }
+
 }
